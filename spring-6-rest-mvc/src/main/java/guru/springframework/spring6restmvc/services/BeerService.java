4eca6964230addd6d0f477f2afbb6714f4f7661d
Katarzyna <kanowory@student.wszib.edu.pl>
2023-02-17 13:23:24 +0100

    Initial commit

---
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+
+import java.util.UUID;
+
+public interface BeerService {
+
+    Beer getBeerById(UUID id);
+
+
+}
