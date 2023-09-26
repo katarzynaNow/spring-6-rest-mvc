07b115c0e3a0a9c434ac1fc77d358bf7b905cd48
Katarzyna <noworyta.katarzyna@wp.pl>
2023-09-26 13:37:37 +0200

    Creating JPA Entities

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
@@ -9,8 +9,8 @@ import java.util.UUID;
 @Data
 public class CustomerDTO {
 
-    private String customerName;
     private UUID id;
+    private String customerName;
     private Integer version;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
