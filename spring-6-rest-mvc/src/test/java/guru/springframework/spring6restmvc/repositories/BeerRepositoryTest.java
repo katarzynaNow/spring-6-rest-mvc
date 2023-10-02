7026cb7ecac0e93f7e9dba848893ec09061054ce
Katarzyna <noworyta.katarzyna@wp.pl>
2023-10-02 11:00:09 +0200

    Database Constraint Validation

--- spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -29,6 +30,21 @@ class BeerRepositoryTest {
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void testSaveBeerNameTooLong(){
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 346787456438795687936564938756986439578629340675490386509643w958654986453986589346459865896439865984356453986")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("75732587537834")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
     }
 
 }
