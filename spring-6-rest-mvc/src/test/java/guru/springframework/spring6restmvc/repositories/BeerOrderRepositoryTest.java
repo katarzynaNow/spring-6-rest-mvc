1b08b03394ce6acb095dc9bec95d60fc09d505f5
Katarzyna <noworyta.katarzyna@wp.pl>
2023-10-17 11:22:09 +0200

    Cascade on Persist

--- spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.BeerOrderShipment;
 import guru.springframework.spring6restmvc.entities.Customer;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,6 +37,9 @@ class BeerOrderRepositoryTest {
         BeerOrder beerOrder = BeerOrder.builder()
                 .customerRef("Test order")
                 .customer(testCustomer)
+                .beerOrderShipment(BeerOrderShipment.builder()
+                        .trackingNumber("12345")
+                        .build())
                 .build();
 
         BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
