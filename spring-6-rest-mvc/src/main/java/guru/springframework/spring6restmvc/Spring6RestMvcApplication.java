dfb20cdabbe8d28df693ab2a753a717e7a3814e7
Katarzyna <noworyta.katarzyna@wp.pl>
2023-11-08 10:16:30 +0100

    Customizing username and password

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 public class Spring6RestMvcApplication {
 
     public static void main(String[] args) {
